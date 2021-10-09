63eb776093251f00147e0e48d1eebaabf55da5e2
kinde Tesfaye <kindetadesse5@gmail.com>
2021-10-09 22:02:16 +0200

    publish

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+export default  {
+   products:[{
+       _id:'1',
+       name: 'Slim Shirt',
+       category: 'Shirts',
+       image: '/images/d6.png',
+       price:150,
+       brand:'Nike',
+       rating: 4.5,
+       numRewiews: 10,
+       countInStock: 6
+   },
+   {
+       _id:'2',
+   name: 'Fit Shirt',
+   category: 'Shirts',
+       image: '/images/d6.png',
+       price:100,
+       brand:'Nike',
+       rating: 4.2,
+       numRewiews: 5,
+       countInStock: 6
+   },
+   {
+       _id:'3',
+   name: 'Best Pants',
+   category: 'Pants',
+       image: '/images/d6.png',
+       price:700,
+       brand:'Nike',
+       rating: 4.5,
+       numRewiews: 8,
+       countInStock: 6
+   },
+   {
+       _id: '4',
+    name: 'Fit Shirt',
+    category: 'Shirts',
+        image: '/images/d6.png',
+        price:1500,
+        brand:'Nike',
+        rating: 4.2,
+        numRewiews: 5,
+        countInStock: 0
+    },
+    {
+        _id: '5',
+    name: 'Best Pants',
+    category: 'Pants',
+        image: '/images/d6.png',
+        price:200,
+        brand:'Nike',
+        rating: 4.5,
+        numRewiews: 8,
+        countInStock: 6
+    }
+] 
+}
